f193b0207f598b74c76dea6850de7d69cd7b185c
linyana <1439713475@qq.com>
2022-04-17 15:43:06 +0800

    update

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="search">
-        <input type="text" id="search_input" />
+        <input type="text" id="search_input" placeholder="搜索" />
+        <button id="search_span">
+            <img src="../assets/images/search.png" alt="图片加载失败" />
+        </button>
     </div>
 </template>
 
@@ -13,14 +16,39 @@ export default {};
     position: absolute;
     left: 20px;
     top: 80px;
-    width: 280px;
-    border: 2px solid #0a8dfc;
+    width: 340px;
 }
 
 input#search_input {
     width: 280px;
     height: 40px;
-    opacity: 0.5;
-    background-color: rgb(0,0,0,0.8);
+    text-indent: 0.6em;
+    font-size: 20px;
+    outline: none;
+    border: 2px solid #0a8dfc;
+    background-color: rgba(23, 65, 105, 0.5);
+    color: #dfe1e1;
+}
+
+input#search_input:focus {
+    outline: none;
+    border: 2px solid #0a8dfc;
+}
+
+button#search_span {
+    cursor: pointer;
+    vertical-align: top;
+    display: inline-block;
+    width: 55px;
+    height: 44px;
+    text-align: center;
+    border: none;
+    background-color: #0a8dfc;
+}
+
+button#search_span img {
+    margin-top: 8px;
+    width: 30px;
+    height: 30px;
 }
 </style>
